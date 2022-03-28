52a76b3ff1764c6266c6cfcf19d1ea6961d8a90c
MVOLOSH <53019492+MVOLOSH@users.noreply.github.com>
2022-03-28 10:39:09 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,309 @@
+import re
+
+"""
+Reads a text file and replaces a pattern inside
+option (str) : -n - suppresses the duplicate rows
+command (str) : s - to replace the text in a file
+replace_str (str): the string to replace
+to_pattern (str): the pattern to replace to
+flag (str/int): g - global or not global / p - print duplicates
+source (str): the text file to read / string to read
+dest (str) : the text file to write
+
+"""
+"""
+no command or flag - first occurrence in a line [1] V
+int  - nth occurrence [2] V
+g  - all occurrences in a line  [3]   V
+int and g - from nth to all [4] V
+p - duplicate the replaced occurrence [5] V
+-n and p - print only the replaced lines [6] V
+"""
+
+
+def sed(option, command, replace_str, to_pattern, flag, source, dest):
+    if re.match("[a-zA-z0-9]*.txt$", source):
+        if re.match("^s$", command):
+            if re.match("^g$", flag) and re.match("^$", option):
+
+                file_read = open(source, "r")
+                data = file_read.read()
+                if replace_str not in data:
+                    # raise IOError("String not found in the text")
+                    print(data)
+                    return
+                else:
+                    data = re.sub(replace_str, to_pattern, data)
+                    file_read.close()
+
+                file_write = open(dest, "w")
+                file_write.write(data)
+                file_write.close()
+                print(data)
+
+            elif re.match("^$", flag) and re.match("^$", option):
+
+                str_result = ""
+                result = []
+                file_read = open(source, "r")
+                data = file_read.read()
+                before = data.split("\n")
+                if replace_str not in data:
+                    # raise IOError("String not found in the text")
+                    print(data)
+                    return
+                else:
+                    for i in before:
+                        result.append(re.sub(replace_str, to_pattern, i, 1))
+                    for x in result:
+                        str_result += x + "\n"
+                    file_read.close()
+
+                file_write = open(dest, "w")
+                file_write.write(str_result)
+                file_write.close()
+                print(data)
+
+            elif re.match("^[0-9]*$", flag) and re.match("^$", option):
+
+                file_read = open(source, "r")
+                data = file_read.read()
+                if replace_str not in data:
+                    # raise IOError("String not found in the text")
+                    print(data)
+                    return
+                else:
+                    new_string = replace_nth(data, replace_str, to_pattern, int(flag[0]), "nth")
+                    file_read.close()
+
+                file_write = open(dest, "w")
+                file_write.write(new_string)
+                file_write.close()
+                print(new_string)
+
+            elif re.match("^[0-9g]*$", flag) and re.match("^$", option):
+
+                file_read = open(source, "r")
+                data = file_read.read()
+                if replace_str not in data:
+                    # raise IOError("String not found in the text")
+                    print(data)
+                    return
+                else:
+                    new_string = replace_nth(data, replace_str, to_pattern, int(flag[0]), "nth_right")
+                    file_read.close()
+                file_write = open(dest, "w")
+                file_write.write(new_string)
+                file_write.close()
+                print(new_string)
+
+            elif re.match("^p$", flag):
+                if re.match("^-n$", option):
+
+                    str_result = ""
+                    result = []
+                    file_read = open(source, "r")
+                    data = file_read.read()
+                    if replace_str not in data:
+                        # raise IOError("String not found in the text")
+                        print(data)
+                        return
+                    else:
+                        before = data.split("\n")
+                        data = re.sub(replace_str, to_pattern, data)
+                        after = data.split("\n")
+
+                        for i, j in zip(after, before):
+                            if i != j:
+                                result.append(i)
+                        for x in result:
+                            str_result += x + "\n"
+                        file_read.close()
+
+                    file_write = open(dest, "w")
+                    file_write.write(str_result)
+                    file_write.close()
+                    print(str_result)
+
+                else:
+                    str_result = ""
+                    result = []
+                    file_read = open(source, "r")
+                    data = file_read.read()
+                    if replace_str not in data:
+                        # raise IOError("String not found in the text")
+                        print(data)
+                        return
+                    else:
+                        before = data.split("\n")
+                        data = re.sub(replace_str, to_pattern, data)
+                        after = data.split("\n")
+
+                        for i, j in zip(after, before):
+                            if i != j:
+                                for k in range(2):
+                                    result.append(i)
+                            else:
+                                result.append(j)
+                        for x in result:
+                            str_result += x + "\n"
+                        file_read.close()
+
+                    file_write = open(dest, "w")
+                    file_write.write(str_result)
+                    file_write.close()
+                    print(str_result)
+            else:
+                return
+
+    else:
+        if re.match("^s$", command):
+            if re.match("^g$", flag) and re.match("^$", option):
+
+                to_replace = input("Write the string to change: ")
+                if replace_str not in to_replace:
+                    # raise IOError("String not found in the text")
+                    print(to_replace)
+                    return
+                else:
+                    new_string = re.sub(replace_str, to_pattern, to_replace)
+
+                    file_write = open(dest, "w")
+                    file_write.write(new_string)
+                    file_write.close()
+                    print(new_string)
+
+            elif re.match("^$", flag) and re.match("^$", option):
+
+                str_result = ""
+                result = []
+                to_replace = input("Write the string to change: ")
+                before = to_replace
+                if replace_str not in before:
+                    # raise IOError("String not found in the text")
+                    print(before)
+                    return
+                else:
+                    result.append(re.sub(replace_str, to_pattern, to_replace, 1))
+                    for x in result:
+                        str_result += x
+
+                file_write = open(dest, "w")
+                file_write.write(str_result)
+                file_write.close()
+                print(str_result)
+
+            elif re.match("^[0-9]*$", flag) and re.match("^$", option):
+
+                to_replace = input("Write the string to change: ")
+                if replace_str not in to_replace:
+                    # raise IOError("String not found in the text")
+                    print(to_replace)
+                    return
+                else:
+                    new_string = replace_nth(to_replace, replace_str, to_pattern, int(flag[0]), "nth")
+                    file_write = open(dest, "w")
+                    file_write.write(new_string)
+                    file_write.close()
+                    print(new_string)
+
+            elif re.match("^[0-9g]*$", flag) and re.match("^$", option):
+
+                to_replace = input("Write the string to change: ")
+                if replace_str not in to_replace:
+                    # raise IOError("String not found in the text")
+                    print(to_replace)
+                    return
+                else:
+                    new_string = replace_nth(to_replace, replace_str, to_pattern, int(flag[0]), "nth_right")
+                    file_write = open(dest, "w")
+                    file_write.write(new_string)
+                    file_write.close()
+                    print(new_string)
+
+            elif re.match("^p$", flag):
+                if re.match("^-n$", option):
+
+                    str_result = ""
+                    result = []
+                    to_replace = input("Write the string to change: ")
+                    if replace_str not in to_replace:
+                        # raise IOError("String not found in the text")
+                        print(to_replace)
+                        return
+                    else:
+                        before = to_replace
+                        new_string = re.sub(replace_str, to_pattern, to_replace)
+                        after = new_string
+                        if before != after:
+                            result.append(after)
+                        else:
+                            result.append(before)
+                        for x in result:
+                            str_result += x + "\n"
+
+                    file_write = open(dest, "w")
+                    file_write.write(str_result)
+                    file_write.close()
+                    print(str_result)
+
+                else:
+                    str_result = ""
+                    result = []
+                    to_replace = input("Write the string to change: ")
+                    if replace_str not in to_replace:
+                        # raise IOError("String not found in the text")
+                        print(to_replace)
+                        return
+                    else:
+                        before = to_replace
+                        new_string = re.sub(replace_str, to_pattern, to_replace)
+                        after = new_string
+                        if before != after:
+                            result.append(after)
+                            result.append(after)
+                        else:
+                            result.append(before)
+                        for x in result:
+                            str_result += x + "\n"
+
+                    file_write = open(dest, "w")
+                    file_write.write(str_result)
+                    file_write.close()
+                    print(str_result)
+            else:
+                return
+
+
+def replace_nth(string, old_str, new_str, n=1, option="nth"):
+    if option == 'nth':
+        left_join = old_str
+        right_join = old_str
+    elif option == 'nth_right':
+        left_join = old_str
+        right_join = new_str
+    else:
+        return
+    groups = string.split(old_str)
+    nth_split = [left_join.join(groups[:n]), right_join.join(groups[n:])]
+    return new_str.join(nth_split)
+
+
+# sample inputs
+"""
+with text file:
+sed("", "s", "cat", "zebra", "", "input.txt", "output.txt")
+sed("", "s", "cat", "zebra", "g", "input.txt", "output.txt")
+sed("", "s", "cat", "zebra", "2", "input.txt", "output.txt")
+sed("", "s", "cat", "zebra", "3g", "input.txt", "output.txt")
+sed("", "s", "cat", "zebra", "p", "input.txt", "output.txt")
+sed("-n", "s", "cat", "zebra", "p", "input.txt", "output.txt")
+
+with string:
+sed("", "s", "cat", "zebra", "", "", "output.txt")
+sed("", "s", "cat", "zebra", "g", "", "output.txt")
+sed("", "s", "cat", "zebra", "1", "", "output.txt")
+sed("", "s", "cat", "zebra", "4g", "", "output.txt")
+sed("", "s", "cat", "zebra", "p", "", "output.txt")
+sed("-n", "s", "cat", "zebra", "p", "", "output.txt")
+"""
